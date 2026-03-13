7fb30b21e38f1d5fee74be5f40cd672a7dc27e3e
sahilgiri.meghanathi <sahilgiri.meghanathi@simformsolutions.com>
2026-03-13 11:28:51 +0530

    Feat : Integrate Sentry for error tracking in the application
    - Added Sentry initialization in the main.tsx file to enable error tracking and monitoring in the application. This will help in identifying and fixing issues more efficiently by providing detailed error reports and insights. The Sentry DSN is configured using environment variables for better security and flexibility.

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,14 @@ import "./index.css";
 import App from "./App.tsx";
 import { store } from "./store/index.ts";
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  sendDefaultPii: true,
+});
+
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
